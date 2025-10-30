52b365f41527f7e46be059938a0121fa0d1b39ff
TenneyHu <tenneyhu@gmail.com>
2025-10-30 17:29:48 +0100

    v1.1

--- src/evaluate.py
+++ src/evaluate.py
@@ -29,6 +29,35 @@ def lexical_diversity(text_dict, max_n=3):
         per_input_avg_ratios.append(avg_ratio)
     return sum(per_input_avg_ratios) / len(per_input_avg_ratios) if per_input_avg_ratios else 0.0
 
+def quality_score(args, queries):
+    device = args.device
+    model_name = args.reward_model
+    rm = AutoModelForSequenceClassification.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        device_map=device,
+        attn_implementation="flash_attention_2",
+        num_labels=1,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    convs = []
+    scores = []
+    for qid, (query, answers) in queries.items():
+        for answer in answers:
+            conv1 = [{"role": "user", "content": query}, {"role": "assistant", "content": answer}]
+            convs.append(conv1)
+    for conv in convs:
+        conv1_formatted = tokenizer.apply_chat_template(conv1, tokenize=False)
+        if tokenizer.bos_token is not None and conv1_formatted.startswith(tokenizer.bos_token):
+            conv1_formatted = conv1_formatted[len(tokenizer.bos_token):]
+        conv1_tokenized = tokenizer(conv1_formatted, return_tensors="pt").to(device)
+        with torch.no_grad():
+            score1 = rm(**conv1_tokenized).logits[0][0].item()
+        scores.append(score1)
+    avg_score = sum(scores) / len(scores) if scores else 0.0
+    print(f"Average Quality Score: {avg_score}")
+    print(f"len scores: {len(scores)}")
+
 def semantic_diversity(model, qid_to_texts):
     diversities = []
     for texts in qid_to_texts.values():
@@ -44,8 +73,11 @@ def semantic_diversity(model, qid_to_texts):
 
 def main():
     parser = argparse.ArgumentParser(description="Compute Diversity")
+    parser.add_argument("--query_path", type=str, default="./data/novelty-bench.txt", help="Path to query file")
     parser.add_argument("--input_path", type=str, default="./results/baselines/nolvelty-bench_gpt-5-mini.txt", help="Path to input JSON file")
-    
+    parser.add_argument("--reward_model", type=str, default="Skywork/Skywork-Reward-V2-Llama-3.1-8B", help="Path to reward model")
+    parser.add_argument("--quality", action="store_true", help="Whether to evaluate quality using reward model")
+    parser.add_argument("--device", type=str, default="cuda:0", help="Device to use for inference")
     args = parser.parse_args()
     
     
@@ -53,6 +85,13 @@ def main():
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
     data = {}
+    queries = {}
+
+    with open(args.query_path, "r", encoding="utf-8") as f:
+        for idx,line in enumerate(f):
+            line = line.strip()
+            queries[idx + 1] = (line, [])
+
     with open(args.input_path, "r", encoding="utf-8") as f:
         for line in f:
             if not line.strip():
@@ -66,10 +105,17 @@ def main():
             if query_id not in data:
                 data[query_id] = []
             data[query_id].append(answer)
+            query = queries.get(int(query_id), "")
+            queries[int(query_id)][1].append(answer)
+
+    if args.quality:
+        quality_score(args, queries)
 
     diversity_score = lexical_diversity(data, max_n=3)
     print(f"Lexical Diversity Score: {diversity_score}")
     print (f"Semantic Diversity Score: {semantic_diversity(model, data)}")
+
+
 
 
 if __name__ == "__main__":
